26fe84a82b9cb2868e313ee2ee4822ce955f4403
sysopfb <sysopfb@gmail.com>
2013-03-10 21:15:16 -0500

    Create strToInt.hs

---
+++ strToInt.hs
@@ -0,0 +1,20 @@
+import Data.Char (digitToInt, isDigit)
+
+loop :: Int -> String -> Int
+loop acc [] = acc
+loop acc (x:xs) = loop acc' xs
+  where
+	acc' = acc * 10 + digitToInt x
+
+strToInt :: String -> Int
+strToInt xs = loop 0 xs
+
+loop' acc [] = acc
+loop' acc (x:xs) 
+	| isDigit x = loop' acc' xs
+	| otherwise = loop' acc xs
+	where
+	acc' = acc * 10 + digitToInt x
+
+strToIntAll :: String -> Int
+strToIntAll xs = loop' 0 xs
